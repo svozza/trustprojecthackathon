5c18652555e0c51bb71c0aaedcc693b7fbde2fcd
Stefano Vozza <svozza@gmail.com>
2016-11-30 11:53:47 +0000

    add author parsing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const hl = require('highland');
 const R = require('ramda');
-const JSONStream = require('JSONStream');
+const cheerio = require('cheerio');
 const path = require('path');
 const request = require('superagent');
 const geohash = require('ngeohash');
@@ -26,37 +26,53 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/validate', (req, res) => {
-  hl(request.get(HIVE_URL)
-      .query({auth: 'trust'})
-      .query({author: 'Kevin Maguire'})
-      .then(R.prop('body')))
-      .toCallback((err, data) => {
-          const locations = R.pathOr([], ['aggregations', 'geohash_1', 'geohash_2', 'geohash_3', 'buckets'], data);
-          const article = R.compose(R.propOr({}, '_source'), R.head, R.pathOr([], ['hits', 'hits']))(data);
-          res.json({
-              article: {
-                  title: article.headline,
-                  published: article.published,
-                  url: article.url
-              },
-              author: {
-                  name: article.author,
-                  id: 'https://twitter.com/Kevin_Maguire',
-                  photo: 'http://i2.mirror.co.uk/incoming/article2876475.ece/ALTERNATES/s98/PROFILE-Kevin-Maguire.png',
-                  locations: R.map(R.compose(R.pick(['longitude', 'latitude']), geohash.decode, R.prop('key')), locations)
-              },
-              organisation: {
-                  name: 'The Mirror',
-                  privacy: 'http://www.mirror.co.uk/privacy-statement',
-                  funding: null,
-                  retractions: 'http://www.mirror.co.uk/corrections-clarifications',
-                  sourcing: null,
-                  mission: 'http://www.trinitymirror.com/our-values',
-                  ethics: null
-              }
-          });
-      });
-
+    hl(request.get(req.body.url + 'z').then(x => {
+        return R.prop('text')(x)
+    }))
+        .map(html => {
+            const $ = cheerio.load(html);
+            const authorLink = $('[rel="author"]');
+            return authorLink.text();
+        })
+        .flatMap(author => {
+            return hl(request.get(HIVE_URL).query({auth: 'trust'}).query({author: author})
+                    .then(R.prop('body')));
+        })
+        .map(data => {
+            const locations = R.pathOr([], ['aggregations', 'geohash_1', 'geohash_2', 'geohash_3', 'buckets'], data);
+            const article = R.compose(R.propOr({}, '_source'), R.head, R.pathOr([], ['hits', 'hits']))(data);
+            return {
+                article: {
+                    title: article.headline,
+                    published: article.published,
+                    url: article.url
+                },
+                author: {
+                    name: article.author,
+                    id: 'https://twitter.com/Kevin_Maguire',
+                    photo: 'http://i2.mirror.co.uk/incoming/article2876475.ece/ALTERNATES/s98/PROFILE-Kevin-Maguire.png',
+                    locations: R.map(R.compose(R.pick(['longitude', 'latitude']), geohash.decode, R.prop('key')), locations)
+                },
+                organisation: {
+                    name: 'The Mirror',
+                    privacy: 'http://www.mirror.co.uk/privacy-statement',
+                    funding: null,
+                    retractions: 'http://www.mirror.co.uk/corrections-clarifications',
+                    sourcing: null,
+                    mission: 'http://www.trinitymirror.com/our-values',
+                    ethics: null
+                }
+            }
+        })
+        .toCallback((err, data) => {
+            if(err != null) {
+                if(err.status != null) res.send(err.status, err.message);
+                else res.send(500, err)
+            }
+            else {
+                res.json(data);
+            }
+        });
 });
 
 app.listen(PORT);
